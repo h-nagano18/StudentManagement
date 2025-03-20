bf6cdb6ba06a552125339e186213ffb569e5441e
h-nagano18 <lap.555fut.hit@gmail.com>
2025-03-20 14:23:39 +0900

    4回目コミット（課題10_全生徒の一覧取得・全コースの一覧取得）

--- src/main/java/StudentRepository.java
+++ src/main/java/StudentRepository.java
@@ -1,30 +1,17 @@
 package raisetech.StudentManagement.repository;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Update;
 import raisetech.StudentManagement.Student;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import raisetech.StudentManagement.StudentsCourses;
 
 @Mapper
 public interface StudentRepository {
 
-  @Select("SELECT * FROM student WHERE name = #{name}")
-  Student searchByName(String name);
+  @Select("SELECT * FROM students")
+  List<Student> searchStudents();
 
-  @Insert("INSERT INTO student (name, age) VALUES (#{name}, #{age})")
-  @Options(useGeneratedKeys = true, keyProperty = "studentId")
-  void registerStudent(int studentId, String name, int age);
-
-  @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
-  void updateStudent(String name, int age);
-
-  @Delete("DELETE FROM student WHERE name = #{name}")
-  void deleteStudent(String name);
-
-  @Select("SELECT * FROM student")
-  List<Student> getAllStudents();
-  }
+  @Select("SELECT * FROM students_courses")
+  List<StudentsCourses> searchstudentscourses();
+}
