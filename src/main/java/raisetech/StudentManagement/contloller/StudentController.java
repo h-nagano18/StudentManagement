b6d70b8974d1c94d40d761444931b279e774c89f
h-nagano18 <lap.555fut.hit@gmail.com>
2025-04-20 15:22:22 +0900

    8回目コミット（課題15_Thymeleafを使ったPOST処理の実装。）

---
+++ src/main/java/raisetech/StudentManagement/contloller/StudentController.java
@@ -0,0 +1,67 @@
+package raisetech.StudentManagement.contloller;
+
+import org.springframework.ui.Model;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import raisetech.StudentManagement.contloller.converter.StudentConverter;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.domain.StudentDetail;
+import raisetech.StudentManagement.service.StudentService;
+
+@Controller
+public class StudentController {
+
+  private StudentService service;
+  private StudentConverter converter;
+
+  @Autowired
+  public StudentController(StudentService service, StudentConverter converter) {
+    this.service = service;
+    this.converter = converter;
+  }
+
+  @GetMapping("/studentList")
+  public String getStudentList(Model model) {
+    List<Student> students = service.searchStudentList();
+    List<StudentsCourses> studentsCourses = service.searchStudentsCourseList();
+
+    model.addAttribute("studentList", converter.convertStudentDetails(students, studentsCourses));
+    return "studentList";
+  }
+
+  @GetMapping("/studentsCourseList")
+  public String getStudentsCourseListPage(Model model) {
+    List<StudentsCourses> courses = service.searchStudentsCourseList();
+    model.addAttribute("studentsCourses", courses);
+    return "studentCourses";
+  }
+
+  @GetMapping("/newStudent")
+  public String newStudent(Model model) {
+    model.addAttribute("studentDetail", new StudentDetail());
+    return "registerStudent";
+  }
+
+  @PostMapping("/registerStudent")
+  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result,
+      Model model) {
+    try {
+      if (result.hasErrors()) {
+        return "registerStudent";
+      }
+
+      service.registerStudent(studentDetail);
+      return "redirect:/studentList";
+
+    } catch (RuntimeException e) {
+      model.addAttribute("errorMessage", e.getMessage());
+      return "registerStudent";
+    }
+  }
+}
