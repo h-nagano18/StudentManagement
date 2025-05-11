91f0448b3b7b31f63cd9b8dafafdf0741ae5f74f
h-nagano18 <lap.555fut.hit@gmail.com>
2025-05-11 14:12:15 +0900

    課題16_受講生情報登録処理の修正と受講生情報更新処理の実装。 (#16)
    * 7回目コミット（課題13_Thymeleafを使ったReadの画面描画処理の実装（受講生一覧の表示）。studentの電話番号項目の追加。）
    
    * 7回目コミット（課題14_Thymeleafを使ったReadの画面描画処理の実装（受講生一覧の表示）。studentの電話番号項目の追加。）
    
    * 8回目コミット（課題15_Thymeleafを使ったPOST処理の実装。）
    
    * 8回目コミット（課題15_Thymeleafを使ったPOST処理の実装。）
    
    * 8回目コミット（課題15_Thymeleafを使ったPOST処理の実装。）
    
    * 9回目コミット（課題16_受講生情報登録処理の修正と受講生情報更新処理の実装。）

--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -1,8 +1,12 @@
 package raisetech.StudentManagement.service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
 import raisetech.StudentManagement.domain.StudentDetail;
@@ -22,21 +26,86 @@ public class StudentService {
     return repository.searchStudents();
   }
 
+  public List<StudentDetail> searchStudentDetailList() {
+    List<Student> students = repository.searchStudents();
+    List<StudentDetail> details = new ArrayList<>();
+
+    for (Student student : students) {
+      List<StudentsCourses> courses = repository.searchStudentsCoursesByStudentId(student.getId());
+      StudentDetail detail = new StudentDetail();
+      detail.setStudent(student);
+      detail.setStudentsCourses(courses);
+      details.add(detail);
+    }
+
+    return details;
+  }
+
   public List<StudentsCourses> searchStudentsCourseList() {
     return repository.searchStudentsCourse();
   }
 
-   public void registerStudent(StudentDetail studentDetail) {
-     Student student = studentDetail.getStudent();
-     repository.insertStudent(student);
+  @Transactional
+  public void registerStudent(StudentDetail studentDetail) {
+    Student student = studentDetail.getStudent();
+    repository.insertStudent(studentDetail.getStudent());
 
-     int generatedId = student.getId();
+    if (studentDetail.getStudentsCourses() != null) {
+      for (StudentsCourses studentsCourse : studentDetail.getStudentsCourses()) {
+        studentsCourse.setStudentId(studentDetail.getStudent().getId());
+        studentsCourse.setStartDate(LocalDateTime.now());
+        studentsCourse.setEndDate(LocalDateTime.now().plusYears(1));
+        repository.insertStudentsCourse(studentsCourse);
+      }
+    }
+  }
 
-     if (studentDetail.getStudentsCourses() != null) {
-       for (StudentsCourses course : studentDetail.getStudentsCourses()) {
-         course.setStudentId(generatedId);
-         repository.insertStudentsCourse(course);
-       }
+  @Transactional
+  public StudentDetail getStudentDetailById(int id) {
+    Student student = repository.searchStudentById(id);
+    List<StudentsCourses> courses = repository.searchStudentsCoursesByStudentId(id);
+    StudentDetail detail = new StudentDetail();
+    detail.setStudent(student);
+    detail.setStudentsCourses(courses);
+    return detail;
+  }
+
+  @Transactional
+  public void updateStudent(StudentDetail studentDetail) {
+    // ① students テーブルは常に更新
+    Student student = studentDetail.getStudent();
+    //更新対象の確認
+    System.out.println("Taget updated studentId: " + studentDetail.getStudent().getId());
+    repository.updateStudent(studentDetail.getStudent());
+
+    // 現在のDBにあるコース情報と比較
+    int studentId = studentDetail.getStudent().getId();
+    List<StudentsCourses> dbCourses = repository.searchStudentsCoursesByStudentId(studentId);
+    List<StudentsCourses> inputCourses = studentDetail.getStudentsCourses();
+
+    if (!isSameCourses(dbCourses, inputCourses)) {
+      repository.deleteStudentsCoursesByStudentId(studentId);
+      for (StudentsCourses studentsCourse : inputCourses) {
+        studentsCourse.setStudentId(studentId);
+        studentsCourse.setStartDate(LocalDateTime.now());
+        studentsCourse.setEndDate(LocalDateTime.now().plusYears(1));
+        repository.insertStudentsCourse(studentsCourse);
+      }
+    }
+  }
+    // 差分があるか比較するヘルパーメソッド
+    private boolean isSameCourses(List<StudentsCourses> dbCourses, List<StudentsCourses> inputCourses) {
+      if (dbCourses.size() != inputCourses.size()) {
+        return false;
      }
+
+     for (int i = 0; i < dbCourses.size(); i++) {
+        StudentsCourses db = dbCourses.get(i);
+       StudentsCourses input = inputCourses.get(i);
+        if (!Objects.equals(db.getCourseName(), input.getCourseName())) {
+          return false;
+        }
+    }
+    return true;
   }
 }
