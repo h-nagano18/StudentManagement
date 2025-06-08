d0120a6b65ba6f714ad920a4e1e804055f62a029
h-nagano18 <lap.555fut.hit@gmail.com>
2025-06-08 16:59:29 +0900

    課題21 (#24)

--- src/main/java/raisetech/StudentManagement/contloller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/contloller/StudentController.java
@@ -1,66 +1,56 @@
 package raisetech.StudentManagement.contloller;
 
-import java.util.Arrays;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import raisetech.StudentManagement.contloller.converter.StudentConverter;
-import raisetech.StudentManagement.data.Student;
-import raisetech.StudentManagement.data.StudentsCourses;
 import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
-
+/**
+ * 受講生の検索や登録、更新を行うREST APIとして受け付けるControllerです。
+ */
 @RestController
 public class StudentController {
 
   private StudentService service;
-  private StudentConverter converter;
 
   @Autowired
-  public StudentController(StudentService service, StudentConverter converter) {
+  public StudentController(StudentService service) {
     this.service = service;
-    this.converter = converter;
   }
 
+  /**
+   * 受講生一覧検索です。
+   * 全件検索を行うので、条件指定は行いません。
+   *
+   * @return　受講生一覧（全件）
+   */
   @GetMapping("/studentList")
   public List<StudentDetail> getStudentList() {
-    List<Student> students = service.searchStudentList();
-    List<StudentsCourses> studentsCourses = service.searchStudentsCourseList();
-    return converter.convertStudentDetails(students, studentsCourses);
-  }
+    return service.searchStudentList();
+   }
 
-  @GetMapping("/studentsCourseList")
-  public String getStudentsCourseListPage(Model model) {
-    List<StudentsCourses> courses = service.searchStudentsCourseList();
-    model.addAttribute("studentsCourses", courses);
-    return "studentCourses";
-  }
-
-  @GetMapping("/newStudent")
-  public String newStudent(Model model) {
-    StudentDetail studentDetail = new StudentDetail();
-    studentDetail.setStudent(new Student());
-    studentDetail.setStudentsCourses(Arrays.asList(new StudentsCourses()));
-    model.addAttribute("studentDetail", studentDetail);
-    return "registerStudent";
+  /**
+   * 受講生検索です。
+   * IDに紐づく任意の受講生の情報を取得します。
+   *
+   * @param id　受講生ID
+   * @return　受講生
+   */
+  @GetMapping("/student/{id}")
+  public StudentDetail getStudent(@PathVariable int id) {
+    return service.searchStudent(id);
   }
 
   @PostMapping("/registerStudent")
-  public ResponseEntity<String> registerStudent(@RequestBody StudentDetail studentDetail) {
-    try {
-      service.registerStudent(studentDetail);
-      return ResponseEntity.ok("登録が成功しました。");
-    } catch (RuntimeException e) {
-      return ResponseEntity.badRequest().body("登録に失敗しました: ");
+  public ResponseEntity<StudentDetail> registerStudent(@RequestBody StudentDetail studentDetail) {
+      StudentDetail responseStudentDetail = service.registerStudent(studentDetail);
+      return ResponseEntity.ok(responseStudentDetail);
     }
-  }
 
   @PostMapping("/updateStudent")
   public ResponseEntity<String> updateStudent(@RequestBody StudentDetail studentDetail) {
